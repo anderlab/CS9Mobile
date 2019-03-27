68ad476bfcc52116e9600e5a6092f4accaccef30
anderlab <anderlaboreo@gmail.com>
2019-03-27 12:18:01 +0100

    some errors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'new', loadChildren: './tab1/new/new.module#NewPageModule' },
+  { path: 'tab6', loadChildren: './tab6/tab6.module#Tab6PageModule' },
+
 ];
 @NgModule({
   imports: [
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  
+  @ViewChild(IonSegment) segment:IonSegment;
+  constructor(){}
+
+  ngOnInit(){
+    this.segment.value='new';
+  }
+ 
+  
+}
